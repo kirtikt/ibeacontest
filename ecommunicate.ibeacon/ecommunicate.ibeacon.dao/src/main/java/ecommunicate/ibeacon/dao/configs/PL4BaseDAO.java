9b031f9db1c97eed90065fd7c135704d1db05347
kirti <kirti.mandwade@wwindia.com>
2016-06-07 20:06:52 +0530

    commiting dao

---
+++ ecommunicate.ibeacon/ecommunicate.ibeacon.dao/src/main/java/ecommunicate/ibeacon/dao/configs/PL4BaseDAO.java
@@ -0,0 +1,29 @@
+package ecommunicate.ibeacon.dao.configs;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class PL4BaseDAO {
+	
+	
+//	private static Logger LOG = Logger.getLogger(PL4BaseDAO.class);
+
+	private static EntityManagerFactory emf = Persistence
+			.createEntityManagerFactory("ibeacon-persistence");
+
+	public static EntityManagerFactory getEmf() {
+		return emf;
+	}
+	
+	public static EntityManager getEm() {
+
+//		LOG.debug(emf);
+		
+		EntityManager em = getEmf().createEntityManager();
+//		LOG.debug("Initialising EntityManager ... ");
+		return em;
+	}
+
+
+}
